698280ff3918f90d9b225e35e407cc4167206897
John Quirke <mullinahone@gmail.com>
2014-04-19 20:53:18 +0100

    Basic Structure
    Basic structure of the look of the first cookie notice

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<title>Cookies notice</title>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+<body>
+<div id="statementDisplay" class="statementdisplay_block">
+<div class="divpoverlay" id="divpoverlay">
+<div class="divoverlay" id="divoverlayid">
+<div class="divsuboverlay" id="divsuboverlay_0">
+<div class="pannelHead" id="pannelHead_0">
+<div> <a href="#" class="closeicon"> </a></div>
+<div class="rtxt"><p class="rtxt_title">Cookie banner</p><p></p>
+<p>This website uses cookies to help us give you the best experience when you visit our website. By continuing to use this website, you consent to our use of these cookies.<br>
+Find out more about how we use cookies and how to manage them by reading our <a href="/content/WebsiteServices/Global/037-simply4doctors-com/en/en/cookie-notice.html">cookie notice</a>.</p>
+<p></p>
+</div>
+<div>
+</div>
+</div>
+</div>
+<div>
+</div>
+</div>
+</div>
+</div>
+	
+</body>
+</head>
+</html>
---
+++ jquery/cookieAlert.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+
+	$('#statementDisplay').hide().show(5000);
+
+});
